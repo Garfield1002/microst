79de22254dd1ff5f85a2dc9d1d5a6aa41ab9d9de
Royer Jack <jack.royer.23@gmail.com>
2022-05-15 21:35:52 +0200

    fixed typos

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup
 
 
-with open("Readme.md") as f:
+with open("Readme.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
 
 setup(
     name="microst",
-    version="0.1.0",
+    version="0.1.1",
     description="micro serial terminal",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Jack Royer",
     author_email="jack.royer.23@gmail.com",
     license="MIT",
-    url="https://github.com/makerdiary/terminal-s",
+    url="https://github.com/Garfield1002/microst",
     entry_points={
         "console_scripts": ["microst=microst.microst:main"],
     },
